9f0a02591b3eff080d969693ca6f11567b0f5922
Joel Kuehne <joelckuehne@gmail.com>
2026-03-30 12:33:58 -0500

    Better benchmarking

--- src/test.cpp
+++ src/test.cpp
@@ -2,10 +2,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 #include <filesystem>
+#include <chrono>
 
 #include "cnf.h"
 
 using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+using namespace std::chrono;
 
 void test_single(std::string path, bool sat)
 {
@@ -22,41 +24,18 @@ void test_single(std::string path, bool sat)
 
 void test_dir(std::string dirpath, bool sat)
 {
-	for (const auto& dirent : recursive_directory_iterator(dirpath))
+	auto start = high_resolution_clock::now();
+	int i = 0;
+
+	for (const auto& dirent : recursive_directory_iterator(dirpath)) {
     	test_single(dirent.path(), sat);
-}
-
-TEST_CASE("cnf uf20-91-1") {
-	const std::vector<cnf::var_state> *sol;
-	cnf::var_state s;
-	cnf::var_t v;
-	cnf cnfsat;
-
-	REQUIRE(cnfsat.init("./test/uf20-91/uf20-01.cnf") == 0);
-	REQUIRE(cnfsat.get_max_var() == 20);
-	REQUIRE(cnfsat.get_clause_cnt() == 91);
-
-	CHECK((void*)cnfsat.solve());
-}
-
-TEST_CASE("cnf uuf75-325") {
-	const std::vector<cnf::var_state> *sol;
-	cnf::var_state s;
-	cnf::var_t v;
-	cnf cnfsat;
-
-	REQUIRE(cnfsat.init("./test/uuf75-325/uuf75-01.cnf") == 0);
-	REQUIRE(cnfsat.get_max_var() == 75);
-	REQUIRE(cnfsat.get_clause_cnt() == 325);
-
-	sol = cnfsat.solve();
-	if (sol != nullptr) {
-		for (v = 0; v < sol->size(); v++) {
-			std::cout << v << "\t" << (uint32_t)s.raw << std::endl;
-		}
+		i += 1;
 	}
 
-	CHECK_FALSE((void*)sol);
+	auto end = high_resolution_clock::now();
+	duration<double, std::milli> elapsed = end - start;
+
+	std::cout << dirpath << " - Average Solve Time: " << elapsed.count() / i << "ms" << std::endl;
 }
 
 TEST_CASE("cnf uf20-91") {
@@ -64,5 +43,5 @@ TEST_CASE("cnf uf20-91") {
 }
 
 TEST_CASE("cnf uuf75-325") {
-	// test_dir("./test/uuf75-325/", false);
+	test_dir("./test/uuf75-325/", false);
 }
